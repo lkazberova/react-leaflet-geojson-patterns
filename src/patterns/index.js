f7bdbb46d33c2cf2f31ccbc8cb66d1516c0864da
Leisan Kazberova <leisan.kh@gmail.com>
2018-11-03 23:28:14 +0400

    CheckerBoardPattern

--- src/patterns/index.js
+++ src/patterns/index.js
@@ -1,6 +1,7 @@
-import './leaflet.pattern';
-import { StripePattern } from './StripePattern';
-import { CirclePattern } from './CirclePattern';
-import { RectPattern } from './RectPattern';
-import { PathPattern } from './PathPattern';
-export default { StripePattern, CirclePattern, RectPattern, PathPattern };
+import './leaflet.pattern'
+import { StripePattern } from './StripePattern'
+import { CirclePattern } from './CirclePattern'
+import { RectPattern } from './RectPattern'
+import { PathPattern } from './PathPattern'
+import {CheckerBoardPattern} from './CheckerBoardPattern'
+export default { StripePattern, CirclePattern, RectPattern, PathPattern, CheckerBoardPattern }
